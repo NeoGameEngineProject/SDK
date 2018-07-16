783a4a0b0ad5a4ca66718df83ea6e06467077674
Yannick Pflanzer <yp1995@outlook.com>
2018-07-16 21:22:49 +0200

    + [NEOENGINE] Implemented behavior properties

--- NeoEngine/include/Behavior.h
+++ NeoEngine/include/Behavior.h
@@ -9,6 +9,29 @@ namespace Neo
 {
 class Object;
 
+class IProperty 
+{
+	std::string m_name;
+public:
+	IProperty(const char* name):
+		m_name(name) {}
+		
+	const std::string& getName() const { return m_name; }
+};
+
+template<typename T>
+class Property : public IProperty
+{
+	T* m_data;
+
+public:
+	Property(const char* name, T& data):
+		IProperty(name), m_data(&data) {}
+
+	T* get() { return m_data; }
+	T& operator*() { return *m_data; }
+};
+
 /**
  * @brief Super class for all behaviors an object might have.
  * 
@@ -19,8 +42,14 @@ class NEO_ENGINE_EXPORT Behavior
 {
 	///< The parent object.
 	Object* m_parent = nullptr;
+	std::vector<IProperty*> m_properties;
+	
 public:
-	virtual ~Behavior() {}
+	virtual ~Behavior() 
+	{
+		for(auto k : m_properties)
+			delete k;
+	}
 	
 	/**
 	 * @brief Returns a static string containing the name of the behavior
@@ -60,6 +89,40 @@ public:
 	
 	Object* getParent() { return m_parent; }
 	void setParent(Object* parent) { m_parent = parent; }
+	
+	/**
+	 * @brief Returns the property belonging to the given name.
+	 * 
+	 * @attention Each behavior needs to override this!
+	 * @param name The name of the property.
+	 * @return The property.
+	 */
+	IProperty* getProperty(const char* name) 
+	{
+		for(auto k : m_properties)
+			if(k->getName() == name)
+				return k;
+			
+		return nullptr;
+	}
+	
+	template<typename T>
+	Property<T>* getProperty(const char* name) { return reinterpret_cast<Property<T>>(getProperty(name)).get(); }
+	
+	template<typename T>
+	T* getProperty(const char* name) { return getProperty<T>(name)->get(); }
+	
+	template<typename T>
+	void setProperty(const char* name, const T& value)
+	{
+		*getProperty<T>(name) = value;
+	}
+	
+	template<typename T>
+	void registerProperty(const char* name, T& data)
+	{
+		m_properties.push_back(new Property<T>(name, data));
+	}
 };
 
 typedef std::unique_ptr<Behavior> BehaviorRef;
