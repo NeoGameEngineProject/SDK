8b6770ee3afea37472e283a2d4c86ff84d4836ca
Yannick Pflanzer <ypflanzer@outlook.com>
2020-01-16 11:21:39 +0100

    * [ NEOENGINE ] Use bigger data type for the depth buffer

--- NeoEngine/platform/graphics/opengl/src/PlatformRenderer.cpp
+++ NeoEngine/platform/graphics/opengl/src/PlatformRenderer.cpp
@@ -75,7 +75,7 @@ void PlatformRenderer::setViewport(unsigned int x, unsigned int y, unsigned int 
 		glBindTexture(GL_TEXTURE_2D, m_pfxDepthTexture);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT16, w, h, 0, GL_DEPTH_COMPONENT, GL_UNSIGNED_BYTE, nullptr);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT32, w, h, 0, GL_DEPTH_COMPONENT, GL_UNSIGNED_BYTE, nullptr);
 		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, m_pfxDepthTexture, 0);
 
 		glGenTextures(1, &m_pfxTexture);
