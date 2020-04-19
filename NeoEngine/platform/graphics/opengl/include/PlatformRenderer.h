3dc08a9380e0190fea4a05e3cf41a1ca30162b2a
Yannick Pflanzer <ypflanzer@outlook.com>
2020-04-19 18:52:52 +0200

    * [ NEOENGINE ] Some style changes

--- NeoEngine/platform/graphics/opengl/include/PlatformRenderer.h
+++ NeoEngine/platform/graphics/opengl/include/PlatformRenderer.h
@@ -6,62 +6,62 @@
 #include <Common.h>
 #include <Mesh.h>
 
-namespace Neo
+namespace Neo 
 {
 
-	class Matrix4x4;
-	class Vector4;
-	class LightBehavior;
-	struct AABB;
-	class MeshBehavior;
-	class Material;
-	class AABBRenderer;
+class Matrix4x4;
+class Vector4;
+class LightBehavior;
+struct AABB;
+class MeshBehavior;
+class Material;
+class AABBRenderer;
 
-	class NEO_ENGINE_EXPORT PlatformRenderer : public Common
-	{
-		unsigned int m_width, m_height;
+class NEO_ENGINE_EXPORT PlatformRenderer : public Common
+{
+	unsigned int m_width, m_height;
+	
+	unsigned short m_visibleLightCount = 0;
+	Array<LightBehavior*> m_visibleLights;
+	Array<Vector4> m_lightBuffer;
+	Array<unsigned char> m_buffer;
+	
+	size_t m_maxVisibleLights = 256;
+	static const unsigned int MAX_LIGHTS_PER_OBJECT = 8;
 
-		unsigned short m_visibleLightCount = 0;
-		Array<LightBehavior*> m_visibleLights;
-		Array<Vector4> m_lightBuffer;
-		Array<unsigned char> m_buffer;
+	CameraBehavior* m_currentCamera = nullptr;
+	unsigned int m_uboLights;
+	
+	unsigned int m_currentFBO = 0, m_pfxFBO = -1, m_pfxTexture = -1, m_pfxDepthTexture = -1, m_pfxVAO = -1, m_pfxVBO = -1, m_pfxUFrustum = -1, m_pfxTime = -1;
+	unsigned int m_pfxShader = -1;
+	unsigned long long m_startTime = 0;
 
-		size_t m_maxVisibleLights = 256;
-		static const unsigned int MAX_LIGHTS_PER_OBJECT = 8;
-
-		CameraBehavior* m_currentCamera = nullptr;
-		unsigned int m_uboLights;
-
-		unsigned int m_currentFBO = 0, m_pfxFBO = -1, m_pfxTexture = -1, m_pfxDepthTexture = -1, m_pfxVAO = -1, m_pfxVBO = -1, m_pfxUFrustum = -1, m_pfxTime = -1;
-		unsigned int m_pfxShader = -1;
-		unsigned long long m_startTime = 0;
-
-		std::vector<Object*> m_opaqueObjects;
-
+	std::vector<Object*> m_opaqueObjects;
+	
 #ifdef DRAW_AABB
-		AABBRenderer* m_aabbRenderer = nullptr;
+	AABBRenderer* m_aabbRenderer = nullptr;
 #endif
-	public:
-		CameraBehavior * getCurrentCamera() { return m_currentCamera; }
-		unsigned long long getTime();
-		void beginFrame(Level& level, CameraBehavior& cam) override;
-		void beginFrame(Neo::CameraBehavior & camera) override;
-		void clear(float r, float g, float b, bool depth) override;
-		void endFrame() override;
-		void initialize(unsigned int w, unsigned int h, void* backbuffer, void* ndt, void* nwh, void* ctx) override;
-		void swapBuffers() override;
-		void setViewport(unsigned int x, unsigned int y, unsigned int w, unsigned int h) override;
-		void compileShaders() override;
+public:
+	CameraBehavior* getCurrentCamera() { return m_currentCamera; }
+	unsigned long long getTime();
+	void beginFrame(Level& level, CameraBehavior& cam) override;
+	void beginFrame(Neo::CameraBehavior & camera) override;
+	void clear(float r, float g, float b, bool depth) override;
+	void endFrame() override;
+	void initialize(unsigned int w, unsigned int h, void* backbuffer, void* ndt, void* nwh, void* ctx) override;
+	void swapBuffers() override;
+	void setViewport(unsigned int x, unsigned int y, unsigned int w, unsigned int h) override;
+	void compileShaders() override;
+	
+	void updateLights(MeshBehavior* mesh);
+	void gatherLights(Array<LightBehavior*>& lights, MeshBehavior* mesh, unsigned short* buffer, unsigned short max, unsigned short& count);
 
-		void updateLights(MeshBehavior* mesh);
-		void gatherLights(Array<LightBehavior*>& lights, MeshBehavior* mesh, unsigned short* buffer, unsigned short max, unsigned short& count);
-
-		void enableMaterial(Neo::Material& material, const Neo::Matrix4x4& ModelView, const Neo::Matrix4x4& ModelViewProjection, const Neo::Matrix4x4& Normal) override;
-
-		void draw(Object* object);
-
-		void setBackbuffer(void* fbo) override { m_currentFBO = (long int)fbo; }
-	};
+	void enableMaterial(Neo::Material& material, const Neo::Matrix4x4& ModelView, const Neo::Matrix4x4& ModelViewProjection, const Neo::Matrix4x4& Normal) override;
+	
+	void draw(Object* object);
+	
+	void setBackbuffer(void* fbo) override { m_currentFBO = (long int) fbo; }
+};
 
 }
 
--- NeoEngine/platform/graphics/opengl/src/PlatformRenderer.cpp
+++ NeoEngine/platform/graphics/opengl/src/PlatformRenderer.cpp
@@ -37,9 +37,9 @@ void PlatformRenderer::beginFrame(Neo::CameraBehavior& camera)
 	camera.enable(m_width, m_height);
 
 	auto* skybox = camera.getParent()->getBehavior<SkyboxBehavior>();
-	if (skybox)
+	if(skybox)
 		skybox->drawSky(this);
-
+	
 	glEnable(GL_DEPTH_TEST);
 	glDepthFunc(GL_LEQUAL);
 	glDisable(GL_BLEND);
@@ -51,7 +51,7 @@ void PlatformRenderer::beginFrame(Neo::CameraBehavior& camera)
 void PlatformRenderer::beginFrame(Level& level, CameraBehavior& cam)
 {
 	glBindFramebuffer(GL_FRAMEBUFFER, m_pfxFBO);
-
+	
 	m_currentCamera = &cam;
 	beginFrame(cam);
 	level.updateVisibility(cam, m_visibleLights);
@@ -59,17 +59,17 @@ void PlatformRenderer::beginFrame(Level& level, CameraBehavior& cam)
 
 void PlatformRenderer::setViewport(unsigned int x, unsigned int y, unsigned int w, unsigned int h)
 {
-	if (m_width != w || m_height != h)
+	if(m_width != w || m_height != h)
 	{
-		if (m_pfxFBO != -1)
+		if(m_pfxFBO != -1)
 			glDeleteFramebuffers(1, &m_pfxFBO);
-
-		if (m_pfxDepthTexture != -1)
+		
+		if(m_pfxDepthTexture != -1)
 			glDeleteTextures(1, &m_pfxDepthTexture);
-
-		if (m_pfxTexture != -1)
+		
+		if(m_pfxTexture != -1)
 			glDeleteTextures(1, &m_pfxTexture);
-
+		
 		glGenFramebuffers(1, &m_pfxFBO);
 		glBindFramebuffer(GL_FRAMEBUFFER, m_pfxFBO);
 
@@ -77,7 +77,7 @@ void PlatformRenderer::setViewport(unsigned int x, unsigned int y, unsigned int 
 		glBindRenderbuffer(GL_RENDERBUFFER, m_pfxDepthTexture);
 		glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, w, h);
 		glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, m_pfxDepthTexture);*/
-
+		
 		glGenTextures(1, &m_pfxDepthTexture);
 		glBindTexture(GL_TEXTURE_2D, m_pfxDepthTexture);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
@@ -91,12 +91,12 @@ void PlatformRenderer::setViewport(unsigned int x, unsigned int y, unsigned int 
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-
+		
 		//gluBuild2DMipmaps(GL_TEXTURE_2D, 3, w, h, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
-
+				
 		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_pfxTexture, 0);
-
+		
 		GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
 		if (status != GL_FRAMEBUFFER_COMPLETE)
 		{
@@ -107,14 +107,14 @@ void PlatformRenderer::setViewport(unsigned int x, unsigned int y, unsigned int 
 		}
 
 		glBindFramebuffer(GL_FRAMEBUFFER, m_currentFBO);
-
+		
 		glUseProgram(m_pfxShader);
-		glUniform2f(glGetUniformLocation(m_pfxShader, "FrameSize"), 1.0f / w, 1.0f / h);
+		glUniform2f(glGetUniformLocation(m_pfxShader, "FrameSize"), 1.0f/w, 1.0f/h);
 	}
-
+	
 	m_width = w;
 	m_height = h;
-
+	
 	glViewport(x, y, w, h);
 }
 
@@ -132,10 +132,10 @@ void PlatformRenderer::updateLights(MeshBehavior* mesh)
 	auto buffer = reinterpret_cast<ShaderLight*>(glMapBuffer(GL_UNIFORM_BUFFER, GL_WRITE_ONLY));
 
 	size_t i = 0;
-	for (i = 0; i < m_visibleLightCount; i++)
+	for(i = 0; i < m_visibleLightCount; i++)
 	{
 		auto light = m_visibleLights[indices[i]];
-		if (light == nullptr)
+		if(light == nullptr)
 			break;
 
 		Object* parent = light->getParent();
@@ -158,12 +158,12 @@ void PlatformRenderer::updateLights(MeshBehavior* mesh)
 
 void PlatformRenderer::clear(float r, float g, float b, bool depth)
 {
-	//	glBindFramebuffer(GL_FRAMEBUFFER, m_pfxFBO);
+//	glBindFramebuffer(GL_FRAMEBUFFER, m_pfxFBO);
 
 	glClearColor(r, g, b, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | (depth ? GL_DEPTH_BUFFER_BIT : 0));
-
-	//	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	
+//	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
 
 unsigned long long PlatformRenderer::getTime()
@@ -171,8 +171,8 @@ unsigned long long PlatformRenderer::getTime()
 	using namespace std::chrono;
 	milliseconds ms = duration_cast<milliseconds>(
 		system_clock::now().time_since_epoch()
-		);
-
+	);
+	
 	return ms.count();
 }
 
@@ -182,45 +182,45 @@ void PlatformRenderer::endFrame()
 	// FIXME Render opaque and transparent objects separately
 	// FIXME Sort by shader
 
-	//	std::sort(m_opaqueObjects.begin(), m_opaqueObjects.end(), [this](Object* o1, Object* o2) {
-	//		const auto camPos = getCurrentCamera()->getParent()->getGlobalPosition();
-	//		return (o1->getGlobalPosition() - camPos).getLength() >= (o2->getGlobalPosition() - camPos).getLength();
-	//	});
-
+//	std::sort(m_opaqueObjects.begin(), m_opaqueObjects.end(), [this](Object* o1, Object* o2) {
+//		const auto camPos = getCurrentCamera()->getParent()->getGlobalPosition();
+//		return (o1->getGlobalPosition() - camPos).getLength() >= (o2->getGlobalPosition() - camPos).getLength();
+//	});
+	
 	// Draw sorted lists
-	for (auto* o : m_opaqueObjects)
+	for(auto* o : m_opaqueObjects)
 	{
 		o->draw(*this);
 	}
 
 #ifdef DRAW_AABB
-	for (auto* o : m_opaqueObjects)
+	for(auto* o : m_opaqueObjects)
 	{
 		m_aabbRenderer->drawBox(*this, o->getTransformedBoundingBox());
 	}
 #endif
-
+	
 	m_opaqueObjects.clear();
-
+	
 	// Finish frame 
 	glBindBuffer(GL_UNIFORM_BUFFER, 0);
 	glBindFramebuffer(GL_FRAMEBUFFER, m_currentFBO);
-
+	
 	useShader(0);
-
+	
 	glUniform2f(m_pfxUFrustum, m_currentCamera->getNear(), m_currentCamera->getFar());
 	glUniform1i(m_pfxTime, getTime() - m_startTime);
-
+	
 	glDisable(GL_DEPTH_TEST);
 	glDisable(GL_BLEND);
-
+	
 	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D, m_pfxTexture);
 	glGenerateMipmap(GL_TEXTURE_2D);
-
+	
 	glActiveTexture(GL_TEXTURE1);
 	glBindTexture(GL_TEXTURE_2D, m_pfxDepthTexture);
-
+	
 	glBindVertexArray(m_pfxVAO);
 	glDrawArrays(GL_TRIANGLE_STRIP, 0, 6);
 	glBindVertexArray(0);
@@ -233,27 +233,27 @@ void PlatformRenderer::initialize(unsigned int w, unsigned int h, void* backbuff
 {
 	m_startTime = getTime();
 	//m_opaqueObjects.reserve(512);
-
+	
 	glewExperimental = true;
 	auto err = glewInit();
-	if (err == GLEW_ERROR_NO_GLX_DISPLAY)
+	if(err == GLEW_ERROR_NO_GLX_DISPLAY)
 	{
 		LOG_INFO("No GLX display found: Using Wayland backend.");
 	}
-	else if (err != GLEW_OK)
+	else if(err != GLEW_OK)
 	{
 		LOG_ERROR("Could not initialize GLEW: " << glewGetErrorString(err));
 		return;
 	}
-
+	
 	auto version = glGetString(GL_VERSION);
 	auto vendor = glGetString(GL_VENDOR);
 	auto renderer = glGetString(GL_RENDERER);
 	auto glslVersion = glGetString(GL_SHADING_LANGUAGE_VERSION);
 
-	m_currentFBO = (long int)backbuffer;
+	m_currentFBO = (long int) backbuffer;
 	LOG_DEBUG("Default FBO: " << m_currentFBO);
-
+	
 	LOG_INFO("Renderer:\t" << renderer);
 	LOG_INFO("Version:\t" << version);
 	LOG_INFO("GLSL Version:\t" << glslVersion);
@@ -267,26 +267,26 @@ void PlatformRenderer::initialize(unsigned int w, unsigned int h, void* backbuff
 	glBindBuffer(GL_UNIFORM_BUFFER, 0);
 
 	m_visibleLights.alloc(m_maxVisibleLights);
-
+	
 	// Create FBO for post fx
 	{
 		m_pfxShader = loadShader("assets/materials/builtin/pfx");
 		glUseProgram(m_pfxShader);
 		glUniform1i(glGetUniformLocation(m_pfxShader, "Color"), 0);
 		glUniform1i(glGetUniformLocation(m_pfxShader, "Depth"), 1);
-
+		
 		m_pfxUFrustum = glGetUniformLocation(m_pfxShader, "Frustum");
 		m_pfxTime = glGetUniformLocation(m_pfxShader, "Time");
 
 		setViewport(0, 0, w, h);
-
+		
 		glGenVertexArrays(1, &m_pfxVAO);
 		glBindVertexArray(m_pfxVAO);
-
+		
 		glGenBuffers(1, &m_pfxVBO);
-
+		
 		Vector2 quad[12] = {
-
+			
 			// Vertices
 			Vector2(-1, -1),
 			Vector2(1, -1),
@@ -295,7 +295,7 @@ void PlatformRenderer::initialize(unsigned int w, unsigned int h, void* backbuff
 			Vector2(-1, -1),
 			Vector2(-1, 1),
 			Vector2(1, 1),
-
+			
 			// Tecoords
 			Vector2(0, 0),
 			Vector2(1, 0),
@@ -307,21 +307,21 @@ void PlatformRenderer::initialize(unsigned int w, unsigned int h, void* backbuff
 
 		glBindBuffer(GL_ARRAY_BUFFER, m_pfxVBO);
 		glBufferData(GL_ARRAY_BUFFER,
-			12 * sizeof(Vector2),
-			quad,
-			GL_STATIC_DRAW);
+					12*sizeof(Vector2),
+					quad,
+					GL_STATIC_DRAW);
 
 		glEnableVertexAttribArray(0);
 		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, nullptr); // position
 
 		glEnableVertexAttribArray(1);
-		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, reinterpret_cast<void*>(sizeof(Vector2) * 6)); // texcoord
+		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, reinterpret_cast<void*>(sizeof(Vector2)*6)); // texcoord
 
 		glBindVertexArray(0);
 	}
-
+	
 	compileShaders();
-
+	
 #ifdef DRAW_AABB
 	m_aabbRenderer = new AABBRenderer();
 	m_aabbRenderer->begin(*this);
@@ -331,17 +331,17 @@ void PlatformRenderer::initialize(unsigned int w, unsigned int h, void* backbuff
 void PlatformRenderer::compileShaders()
 {
 	Common::compileShaders();
-
-	for (auto& shader : getShaders())
+	
+	for(auto& shader : getShaders())
 	{
 		glUseProgram(shader.id);
 		auto uboBind = glGetUniformBlockIndex(shader.id, "Lights");
-		if (uboBind == -1)
+		if(uboBind == -1)
 		{
 			LOG_DEBUG("No light UBO found for " << shader.name.str());
 			continue;
 		}
-
+		
 		const auto bindingPoint = 2;
 		glBindBufferBase(GL_UNIFORM_BUFFER, bindingPoint, m_uboLights);
 		glUniformBlockBinding(shader.id, uboBind, bindingPoint);
@@ -351,7 +351,7 @@ void PlatformRenderer::compileShaders()
 void PlatformRenderer::enableMaterial(Neo::Material& material, const Neo::Matrix4x4& ModelView, const Neo::Matrix4x4& ModelViewProjection, const Neo::Matrix4x4& Normal)
 {
 	Common::enableMaterial(material, ModelView, ModelViewProjection, Normal);
-
+	
 	auto* shader = getShader(material.getShader());
 	glUniform1i(shader->uNumLights, m_visibleLightCount);
 }
@@ -369,13 +369,13 @@ void PlatformRenderer::swapBuffers()
 
 void PlatformRenderer::gatherLights(Array<LightBehavior*>& lights, MeshBehavior* mesh, unsigned short* buffer, unsigned short max, unsigned short& count)
 {
-	for (size_t i = 0; i < lights.count && lights[i] != nullptr && count < max; i++)
+	for(size_t i = 0; i < lights.count && lights[i] != nullptr && count < max; i++)
 	{
 		auto parent = lights[i]->getParent();
 		float distance = (mesh->getParent()->getGlobalPosition() - parent->getGlobalPosition()).getLength() - mesh->getBoundingBox().getDiameter() / 2.0f;
 		float radius = sqrt(1.0f / (lights[i]->attenuation * 0.15f)); // 0.15 = Minimum brightness
-
-		if (distance <= radius)
+		
+		if(distance <= radius)
 		{
 			buffer[count] = i;
 			count++;
